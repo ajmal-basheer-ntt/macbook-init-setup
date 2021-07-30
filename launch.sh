8efb1da31fce23031cf548b9a606b648fa858d87
ajmal2808 <40166706+ajmal2808@users.noreply.github.com>
2021-07-30 13:06:49 +0200

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,11 @@
+xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+pip install aws2-wrap
+
+#docker
+#helm
+#kubectl
+#ansible
+#terraform
